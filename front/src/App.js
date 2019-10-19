e3e3ce2ca91f69958cc6960e5802ef55bc3d84c6
4rfel <rafael_1999_@hotmail.com.br>
2019-10-19 10:08:58 -0300

    aa

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,54 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 // import api from "./services/api";
 import lol from "riot-lol";
-import MultiSelect from 'react-native-multiple-select';
 
 
 
 function App() {
   const types = [
-    {"id":"Fighter", "name":"Fighter"}, 
-    {"id":"Mage", "name":"Mage"},
-    {"id":"Assassin", "name":"Assassin"}, 
-    {"id":"Support", "name":"Support"}, 
-    {"id":"Marksman", "name":"Tank"}
-  ];
-  var selectedItems = [] 
+    {"type": "Fighter"},
+    {"type": "Tank"},
+    {"type": "Mage"},
+    {"type": "Assassin"},
+    {"type": "Marksman"}]
+  // const types = ["Fighter", "Tank", "Mage", "Assassin", "Support", "Marksman"];
 
-  const [currency, setCurrency] = useState(defaultCurrency);
+  const [type1, setType1] = useState("Escolha uma categoria");
+  const [type2, setType2] = useState("Escolha uma categoria");
 
-  onSelectedItemsChange = selectedItems => {
-    this.setState({ selectedItems });
-  };
 
   lol.getChampions().then(champions => {
     console.log(champions);
   });
     
-  return(<h1>aa</h1>
+  const onType1Select = e => {
+    setType1(e.target.value);
+  };
+  const onType2Select = e => {
+    setType2(e.target.value);
+  };
+  return(
+<div>
+  <h1>oi mayra</h1>
+  <select value={type1} onChange={onType1Select}>
+      {types.map((obj, index) => {
+        return (
+          <option key={`${index}-${obj.type}`} value={obj.type}>
+            {obj.type}{" "}
+          </option>
+        );
+      })}
+  </select>
+  <select value={type2} onChange={onType2Select}>
+    {types.map((obj, index) => {
+      return (
+        <option key={`${index}-${obj.type}`} value={obj.type}>
+          {obj.type}{" "}
+        </option>
+      );
+    })}
+  </select>
+</div>
   )
 }
 export default App;
